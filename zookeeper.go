290fa626ada9d6522eff288847b5912f323443e4
Zach Dicklin <zdicklin@gmail.com>
2014-11-23 13:25:00 -0700

    Maintain capture status

--- zookeeper.go
+++ zookeeper.go
@@ -10,9 +10,11 @@ import (
   "strconv"
   "io"
   "time"
+  "encoding/json"
 )
 
 const photoDir string = "./photos"
+const capturingFilename string = "capture_status.txt"
 
 func newPhotoFilename() string {
   now := time.Now()
@@ -31,6 +33,7 @@ func postPhotoHandler(w http.ResponseWriter, r *http.Request) {
   err := r.ParseMultipartForm(100000)
 
   if err != nil {
+      fmt.Println(err.Error())
       http.Error(w, err.Error(), http.StatusInternalServerError)
       return
   }
@@ -41,6 +44,25 @@ func postPhotoHandler(w http.ResponseWriter, r *http.Request) {
 
   dst, _ := os.Create(photoDir + "/" + newPhotoFilename())
   io.Copy(dst, file);
+}
+
+func getCaptureHandler(w http.ResponseWriter, r *http.Request) {
+  mapCapture := map[string]bool{"capturing": readCaptureStatus()}
+  jsonCapture, _ := json.Marshal(mapCapture)
+
+  w.Header().Set("Content-Type", "application/json")
+  fmt.Fprintf(w, string(jsonCapture))
+}
+
+func postCaptureHandler(w http.ResponseWriter, r *http.Request) {
+  capturing, _ := strconv.ParseBool(r.FormValue("capture"))
+  storeCaptureStatus(capturing)
+
+  mapCapture := map[string]bool{"capturing": capturing}
+  jsonCapture, _ := json.Marshal(mapCapture)
+
+  w.Header().Set("Content-Type", "application/json")
+  fmt.Fprintf(w, string(jsonCapture))  
 }
 
 type ByTime []os.FileInfo
@@ -70,10 +92,27 @@ func latestFile() os.FileInfo {
   return files[0]
 }
 
+func storeCaptureStatus(capturing bool) bool { 
+  c :=  []byte(strconv.FormatBool(capturing))
+  err := ioutil.WriteFile(capturingFilename, c, 0644)
+  return err != nil
+}
 
-func main() {
+func readCaptureStatus() bool {
+  dat, err := ioutil.ReadFile(capturingFilename)
+  if err != nil {
+    return false
+  }
+  c, _ := strconv.ParseBool(string(dat))
+  return c
+}
+
+
+func main() {  
   http.HandleFunc("/latest_photo", latestPhotoHandler)
   http.HandleFunc("/post_photo", postPhotoHandler)
+  http.HandleFunc("/update_capture", postCaptureHandler)
+  http.HandleFunc("/capture_status", getCaptureHandler)
 
   http.ListenAndServe(":8080", nil)
 }
